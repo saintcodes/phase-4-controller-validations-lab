6fc5622e23c63d66fbb4dfc34dc582229949e005
Michelle <clairsin@gmail.com>
2022-06-30 17:48:03 -0400

    commit tree ecc644110705ddb884ecbc07c34af408acbb0c9e

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,18 @@
 class AuthorsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   
+  def index
+    authors = Author.all
+    render json: authors, status: :ok
+  end
+
   def show
     author = Author.find(params[:id])
-
-    render json: author
+    render json: author, status: :ok
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -17,5 +21,9 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def render_unprocessable_entity(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
+
 end
